c53d7fd94525601b1b4b5eaeabc521c4cede842f
Bill Lin <bpma@ms96.url.com.tw>
2015-11-27 23:03:07 +0800

    builder pattern

---
+++ Student Homework/BillLin/Control/DTO/Creational/Builder/BuilderDto.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hsdc.dpt.Control.DTO.Creational.Builder
+{
+    public class Leave
+    {
+        public String LeaveID { get; set; }
+        public String Applier { get; set; }
+        public String LeaveType { get; set; }
+        public int LeaveDay { get; set; }
+        public DateTime LeaveBeginDate { get; set; }
+    }
+
+    public class ApplyExpense
+    {
+        public String ApplyExpenseID { get; set; }
+        public String Applier { get; set; }
+        public String ApplyExpenseType { get; set; }
+        public decimal TotalExpense { 
+            get 
+            {
+                decimal exp = 0;
+                if (LineItems != null)
+                    LineItems.ForEach(d => exp += d.Expense);
+                return exp;
+            } 
+        }
+        public DateTime ApplyDate { get; set; }
+        public List<ApplyExpenseDetail> LineItems { get; set; }
+        public String Comment { get; set; }
+    }
+
+    public class ApplyExpenseDetail
+    {
+        public String ReferenceNumber { get; set; }
+        public DateTime OccureDate { get; set; }
+        public decimal Expense { get; set; }
+    }
+
+    public class OverTime
+    {
+        public String OverTimeId { get; set; }
+        public String WorkderId { get; set; }
+        public DateTime WorkDate { get; set; }
+        
+    }
+}
---
+++ Student Homework/BillLin/WebMVCTests/Controllers/Creational/BuilderControllerTests.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WebMVC.Controllers.Creational;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+using hsdc.dpt.Control.Creational.Builder;
+using hsdc.dpt.Control.DTO.Creational.Builder;
+using ExpectedObjects;
+
+namespace WebMVC.Controllers.Creational.Tests
+{
+    [TestClass()]
+    public class BuilderControllerTests
+    {
+        [TestMethod()]
+        public void testNewLeave()
+        {
+            LeaveBuilder builder = new LeaveBuilder();
+            EOfficeDirector<Leave> director = new EOfficeDirector<Leave>();
+
+            Leave actual = director.Construct(builder);          
+            Leave expected = getExpectedLeave();
+
+            expected.ToExpectedObject().ShouldEqual(actual);
+        }
+
+        private Leave getExpectedLeave()
+        {
+            Leave result = new Leave();
+            
+            return result;
+        }
+
+        [TestMethod()]
+        public void testNewOverTime()
+        {
+            OverTimeBuilder builder = new OverTimeBuilder();
+            EOfficeDirector<OverTime> director = new EOfficeDirector<OverTime>();
+     
+            OverTime actual = director.Construct(builder);
+            OverTime expected = new OverTime();
+
+            expected.ToExpectedObject().ShouldEqual(actual);
+        }
+
+        [TestMethod()]
+        public void testSaveOverTime()
+        {
+            SaveOverTimeControl uco = new SaveOverTimeControl();
+
+            OverTime actual = new OverTime();
+            actual = uco.Save(actual);
+
+            OverTime expected = new OverTime();
+            expected.OverTimeId = "0001";
+
+            expected.ToExpectedObject().ShouldEqual(actual);
+
+        }
+
+
+
+    }
+}
