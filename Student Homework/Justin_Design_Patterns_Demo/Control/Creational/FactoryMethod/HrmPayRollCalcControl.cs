57d7e8ab118f7b301252455f1ea788685ef29351
chang Justin <chang Justin>
2015-11-30 22:39:54 +0800

    no message

---
+++ Student Homework/Justin_Design_Patterns_Demo/Control/Creational/FactoryMethod/HrmPayRollCalcControl.cs
@@ -0,0 +1,66 @@
+﻿using hsdc.dpt.Control.DTO.Creational.FactoryMethod;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hsdc.dpt.Control.Creational.FactoryMethod
+{
+    public class HrmPayRollCalcControl
+    {
+
+        public List<PayRoll> CalcPayRoll()
+        {
+            var payrolllist = new List<PayRoll>();
+            var paystructure = GetPayStructure();
+            PayCalc<PayItem> rule = null;
+            Double calcbase = 0;
+            foreach (var item in paystructure)
+            {
+                if (item.PayItemIditity == "001") calcbase = item.value;
+                switch (item.PayRuleIditity)
+                {
+                    case "C001":
+                        rule = new AddPayCalc();
+                        break;
+                    case "C002":
+                        rule = new MinusPayCalc();
+                        break;
+                    case "C003":
+                        rule = new LaborPayCalc();
+                        break;
+                    case "C004":
+                        rule = new NHIPayCalc();
+                        break;
+                    case "C005":
+                        rule = new PensionPayCalc();
+                        break;
+                    case "C006":
+                        rule = new TotalPayCalc();
+                        break;
+                }
+                rule.CalcPayRoll(ref payrolllist, item);
+            }
+            return payrolllist;
+        }
+
+
+
+
+        public List<PayItem> GetPayStructure()
+        {
+            var paystructure = new List<PayItem>();
+            paystructure.Add(new PayItem { PayItemIditity = "001", PayItemName = "本薪", value = 30000, PayRuleIditity = "C001" });
+            paystructure.Add(new PayItem { PayItemIditity = "002", PayItemName = "伙食津貼", value = 1500, PayRuleIditity = "C001" });
+            paystructure.Add(new PayItem { PayItemIditity = "003", PayItemName = "勞保", value = 0.01, PayRuleIditity = "C003" });
+            paystructure.Add(new PayItem { PayItemIditity = "004", PayItemName = "健保", value = 0.01, PayRuleIditity = "C004" });
+            paystructure.Add(new PayItem { PayItemIditity = "005", PayItemName = "勞退", value = 0.06, PayRuleIditity = "C005" });
+            paystructure.Add(new PayItem { PayItemIditity = "006", PayItemName = "應付薪資", value = 0, PayRuleIditity = "C006" });
+
+            return paystructure;
+        }
+
+
+    }
+}
---
+++ Student Homework/Justin_Design_Patterns_Demo/WebMVC/Controllers/Creational/HrmPayCalcController.cs
@@ -0,0 +1,27 @@
+﻿using hsdc.dpt.Control.Creational.FactoryMethod;
+using hsdc.dpt.Control.DTO.Creational.FactoryMethod;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebMVC.Controllers.Creational
+{
+    public class HrmPayCalcController : Controller
+    {
+        // GET: HrmPayCalc
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult PayCalc()
+        {
+            var control = new HrmPayRollCalcControl();
+            List<PayRoll> payStructure = control.CalcPayRoll();
+            return View(payStructure);
+        }
+
+    }
+}
