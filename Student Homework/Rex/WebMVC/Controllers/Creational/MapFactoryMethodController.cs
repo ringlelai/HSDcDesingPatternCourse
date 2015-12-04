2b90e375ac4aca861b9cc377a4000d50ba9730c5
rexlucien <rexlucien@hotmail.com>
2015-12-04 14:52:47 +0800

    change branch

---
+++ Student Homework/Rex/WebMVC/Controllers/Creational/MapFactoryMethodController.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+using System.Web.Mvc;
+
+namespace WebMVC.Controllers.Creational
+{
+    public class MapFactoryMethodController : Controller
+    {
+        // GET: MapFactoryMethod
+        public ActionResult Index()
+        {
+            return View("Index");
+        }
+
+        public ActionResult MapService()
+        {
+            return View("MapService");
+        }
+    }
+
+    public interface IMapService<T>
+    {
+    }
+
+    //public class GoogleMapService : IMapService<>
+    //{
+    //}
+
+    //public class BingMapService : IMapService<>
+    //{
+    //}
+
+    public interface ILocationMarker
+    {
+        string Id { get; set; }
+
+        string Name { get; set; }
+
+        decimal Lat { get; set; }
+
+        decimal Lng { get; set; }
+    }
+
+    public interface IMapView
+    {
+        IEnumerable<ILocationMarker> Markers { get; set; }
+
+        string ViewName { get; set; }
+    }
+}
