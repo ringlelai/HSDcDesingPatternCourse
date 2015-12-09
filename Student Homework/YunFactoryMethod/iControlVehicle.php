75c6762fe5a7a5fa95e01b918938ccc18cfdf152
Bill Lin <bpma@ms96.url.com.tw>
2015-12-09 21:21:14 +0800

    remove from my personal folder

--- Student Homework/YunFactoryMethod/iControlVehicle.php
+++
@@ -1,9 +0,0 @@
-<?php
-interface iControlVehicle{
-	public function launchVehicle();
-	public function left();
-	public function right();
-	public function forward();
-	public function back();
-	public function stopVehicle();
-}
