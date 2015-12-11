3e532dd86b8b560cd61b214cd74077026828957f
Bill Lin <bpma@ms96.url.com.tw>
2015-12-11 10:27:32 +0800

    remove my personal files

--- Student Homework/BillLin/JavaExample/Control/src/tw/bill/creation/factorymethod/Telegram.java
+++
@@ -1,9 +0,0 @@
-package tw.bill.creation.factorymethod;
-
-import java.util.List;
-
-// 可延伸成template method
-public abstract class Telegram<I, O> {
-	public abstract List<O> fetchAll();
-	public abstract void send(I obj);
-}
