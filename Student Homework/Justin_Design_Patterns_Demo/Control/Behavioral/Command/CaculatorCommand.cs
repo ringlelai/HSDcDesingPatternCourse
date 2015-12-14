53265404e9d363b09198558a962cf60652118d81
chang Justin <chang Justin>
2015-12-14 23:45:39 +0800

    Command加入UNDO

--- Student Homework/Justin_Design_Patterns_Demo/Control/Behavioral/Command/CaculatorCommand.cs
+++ Student Homework/Justin_Design_Patterns_Demo/Control/Behavioral/Command/CaculatorCommand.cs
@@ -9,6 +9,7 @@ namespace hsdc.dpt.Control.Behavioral.Command.Caculator
     public class CalculationInvoker
     {
         private List<Command> commands = new List<Command>();
+        private int current = 0;
         public void Compute(object receiver, string @operator, int operand)
         {
             // Create command operation and execute it
@@ -16,6 +17,18 @@ namespace hsdc.dpt.Control.Behavioral.Command.Caculator
               receiver, @operator, operand);
             command.Execute();
             commands.Add(command);
+            current++;
+        }
+        public void Undo(int levels)
+        {
+            for(int i =0;i< levels;i++)
+            {
+                if(current>0)
+                {
+                    Command command = commands[--current] as Command;
+                    command.UnExecute();
+                }
+            }
         }
     }
 
@@ -24,6 +37,7 @@ namespace hsdc.dpt.Control.Behavioral.Command.Caculator
         protected string Operator;
         protected int Operand;
         public abstract void Execute();
+        public abstract void UnExecute();
         public Command(object receiver, string Operator, int Operand) 
         {
             this.Operator = Operator;
@@ -45,25 +59,67 @@ namespace hsdc.dpt.Control.Behavioral.Command.Caculator
             if (this._caculator != null)
                 this._caculator.Operation(Operator, Operand);
         }
-    }
+        public override void UnExecute()
+        {
+            if (this._caculator != null)
+                this._caculator.Operation(Undo(Operator), Operand);
+        }
 
-    [Serializable]
-    public class ElementaryArithCaculator
-    {
-        private int _curr = 0;
-
-        public void Operation(string @operator, int operand)
+        private string Undo(string @operator)
         {
             switch (@operator)
             {
-                case "+": _curr += operand; break;
-                case "-": _curr -= operand; break;
-                case "*": _curr *= operand; break;
-                case "/": _curr /= operand; break;
+                case "+":
+                    @operator = "-";
+                    break;
+                case "-":
+                    @operator = "+";
+                    break;
+                case "*":
+                    @operator = "/";
+                    break;
+                case "/":
+                    @operator = "*";
+                    break;
             }
+            return @operator;
+        }
+    }
+
+    public abstract class ICaculator
+    {
+        public abstract int GetResult();
+        public abstract void Operation(string @operator, int operand);
+    }
+
+
+    [Serializable]
+    public class ElementaryArithCaculator:ICaculator
+    {
+        private int _curr = 0;
+
+        public override void Operation(string @operator, int operand)
+        {
+            //http://www.dofactory.com/net/command-design-pattern
+            switch (@operator)
+            {
+                case "+":
+                    _curr += operand;
+                    break;
+                case "-":
+                    _curr -= operand;
+                    break;
+                case "*":
+                    _curr *= operand;
+                    break;
+                case "/":
+                    _curr /= operand;
+                    break;
+            }
+
         }
 
-        public int GetResult()
+        public override int GetResult()
         {
             return _curr;
         }
